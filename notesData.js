75715865263a234fd9749dc92ec57f1b9f6c2207
aniketgitsub <andjhdur@gmail.com>
2025-10-13 14:02:44 +0530

    Add files via upload

---
+++ notesData.js
@@ -0,0 +1,85 @@
+// notesData.js फ़ाइल
+const subjects = [
+    { 
+        title: '🌍 Geography', 
+        color: 'violet', 
+        notes: [
+            // Geography Notes
+            { name: '🌌 SOLAR SYSTEM Notes', link: 'https://drive.google.com/file/d/1oga0Z6CXNJh31XiK1sp9g6c_sLHsaqTe/view?usp=drive_link' },
+            { name: '📍 LATITUDE AND LONGITUDE', link: 'https://drive.google.com/file/d/1GPA4WC5YbMdLASvmTjgK9IXUBGlTQyHs/view?usp=drive_link' },
+            { name: '🌋 ROCKS AND VOLCANOES', link: 'https://drive.google.com/file/d/1UWh1MSoa5CxRl37pLbkERXWeALuu0VoR/view?usp=drive_link' },
+            { name: "🌍 EARTH'S INTERIOR & PLATE TECHTONICS", link: 'https://drive.google.com/file/d/1jOtWa67ev4oHCO21J-YKB0iClP_y0NR5/view?usp=drive_link' },
+            { name: '🗺️ CONTINENT AND OCEAN ', link: 'https://drive.google.com/file/d/1XI07VXPzW63qpiXDxZ4wEfTU5cvSHdEP/view?usp=drive_link' }
+        ] 
+    },
+    { 
+        title: '🏺 Ancient History', 
+        color: 'indigo', 
+        notes: [
+           
+        ] 
+    },
+    { 
+        title: '⚔️ Medieval History', 
+        color: 'blue', 
+        notes: [
+            
+        ] 
+    },
+    { 
+        title: '🇮🇳 Modern History', 
+        color: 'green', 
+        notes: [
+           
+        ] 
+    },
+    { 
+        title: '📜 Polity', 
+        color: 'yellow', 
+        notes: [
+            
+        ] 
+    },
+    { 
+        title: '💰 Economics', 
+        color: 'orange', 
+        notes: [
+            
+        ] 
+    },
+    { 
+        title: '🧲 Physics', 
+        color: 'red', 
+        notes: [
+           
+        ] 
+    },
+    { 
+        title: '⚗️ Chemistry', 
+        color: 'violet', 
+        notes: [
+            
+        ] 
+    },
+    { 
+        title: '🧬 Biology', 
+        color: 'indigo', 
+        notes: [
+            
+        ] 
+    },
+    { 
+        title: '🌱 Environment', 
+        color: 'blue', 
+        notes: [
+            
+        ] 
+    },
+    { 
+        title: '📚 Static GK', 
+        color: 'green', 
+        notes: [
+            
+        ] 
+    }
+];
